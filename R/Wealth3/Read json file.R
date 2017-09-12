37ada2a160339d45b62010dbdbc7b9015035b189
Angelo Zanella <ajzanella@gmail.com>
2017-09-12 21:39:00 +1000

    Json file trello
    File to read json file from trello

---
+++ R/Wealth3/Read json file.R
@@ -0,0 +1,225 @@
+	library(jsonlite)
+	library(stringr)
+
+	#trello.json.data<-fromJSON("C:/Users/ajzan/Desktop/trello file export.json", flatten=TRUE)
+	trello.json.data<-fromJSON("C:/Users/ajzan/Desktop/trello file export2.json", flatten=TRUE)
+
+	#######################
+	### set up variable ###
+	#######################
+		
+		#Card
+		card_id<-trello.json.data$actions$id
+		card_idMemberCreator<-trello.json.data$actions$idMemberCreator
+		card_type<-trello.json.data$actions$type
+		card_date<-trello.json.data$actions$date
+		card_data_text<-trello.json.data$actions$data.text
+		card_data_idMember<-trello.json.data$actions$data.idMember
+		card_data_dateLastEdited<-trello.json.data$actions$data.dateLastEdited
+		card_data_idMemberAdded<-trello.json.data$actions$data.idMemberAdded
+		card_data_memberType<-trello.json.data$actions$data.memberType
+		card_data_idOriginalCommenter<-trello.json.data$actions$data.idOriginalCommenter
+		card_data_deactivated<-trello.json.data$actions$data.deactivated
+		card_data_checkItem_textData<-trello.json.data$actions$data.checkItem.textData
+		card_data_checkItem_state<-trello.json.data$actions$data.checkItem.state
+		card_data_checkItem_name<-trello.json.data$actions$data.checkItem.name
+		card_data_checkItem_id<-trello.json.data$actions$data.checkItem.id
+		card_data_checklist_name<-trello.json.data$actions$data.checklist.name
+		card_data_checklist_id<-trello.json.data$actions$data.checklist.id
+		card_data_card_shortLink<-trello.json.data$actions$data.card.shortLink
+		card_data_card_idShort<-trello.json.data$actions$data.card.idShort
+		card_data_card_name<-trello.json.data$actions$data.card.name
+		card_data_card_id<-trello.json.data$actions$data.card.id
+		card_data_card_due<-trello.json.data$actions$data.card.due
+		card_data_card_dueComplete<-trello.json.data$actions$data.card.dueComplete
+		card_data_card_pos<-trello.json.data$actions$data.card.pos
+		card_data_card_idList<-trello.json.data$actions$data.card.idList
+		card_data_card_closed<-trello.json.data$actions$data.card.closed
+		card_data_card_desc<-trello.json.data$actions$data.card.desc
+		card_data_board_shortLink<-trello.json.data$actions$data.board.shortLink
+		card_data_board_name<-trello.json.data$actions$data.board.name
+		card_data_board_id<-trello.json.data$actions$data.board.id
+		card_data_board_prefs_permissionLevel<-trello.json.data$actions$data.board.prefs.permissionLevel
+		card_data_board_prefs_selfJoin<-trello.json.data$actions$data.board.prefs.selfJoin
+		card_data_list_name<-trello.json.data$actions$data.list.name
+		card_data_list_id<-trello.json.data$actions$data.list.id
+		card_data_list_closed<-trello.json.data$actions$data.list.closed
+		card_data_list_pos<-trello.json.data$actions$data.list.pos
+		card_data_old_due<-trello.json.data$actions$data.old.due
+		card_data_old_dueComplete<-trello.json.data$actions$data.old.dueComplete
+		card_data_old_name<-trello.json.data$actions$data.old.name
+		card_data_old_pos<-trello.json.data$actions$data.old.pos
+		card_data_old_idList<-trello.json.data$actions$data.old.idList
+		card_data_old_closed<-trello.json.data$actions$data.old.closed
+		card_data_old_desc<-trello.json.data$actions$data.old.desc
+		card_data_old_prefs_permissionLevel<-trello.json.data$actions$data.old.prefs.permissionLevel
+		card_data_old_prefs_selfJoin<-trello.json.data$actions$data.old.prefs.selfJoin
+		card_data_listAfter_name<-trello.json.data$actions$data.listAfter.name
+		card_data_listAfter_id<-trello.json.data$actions$data.listAfter.id
+		card_data_listBefore_name<-trello.json.data$actions$data.listBefore.name
+		card_data_listBefore_id<-trello.json.data$actions$data.listBefore.id
+		card_data_attachment_url<-trello.json.data$actions$data.attachment.url
+		card_data_attachment_name<-trello.json.data$actions$data.attachment.name
+		card_data_attachment_id<-trello.json.data$actions$data.attachment.id
+		card_data_attachment_previewUrl<-trello.json.data$actions$data.attachment.previewUrl
+		card_data_attachment_previewUrl2x<-trello.json.data$actions$data.attachment.previewUrl2x
+		card_data_cardSource_shortLink<-trello.json.data$actions$data.cardSource.shortLink
+		card_data_cardSource_idShort<-trello.json.data$actions$data.cardSource.idShort
+		card_data_cardSource_name<-trello.json.data$actions$data.cardSource.name
+		card_data_cardSource_id<-trello.json.data$actions$data.cardSource.id
+		card_data_boardSource_id<-trello.json.data$actions$data.boardSource.id
+		card_data_plugin_url<-trello.json.data$actions$data.plugin.url
+		card_data_plugin_public<-trello.json.data$actions$data.plugin.public
+		card_data_plugin_name<-trello.json.data$actions$data.plugin.name
+		card_data_plugin_id<-trello.json.data$actions$data.plugin.id
+		card_data_member_name<-trello.json.data$actions$data.member.name
+		card_data_member_id<-trello.json.data$actions$data.member.id
+		card_data_boardTarget_id<-trello.json.data$actions$data.boardTarget.id
+		card_data_organization_name<-trello.json.data$actions$data.organization.name
+		card_data_organization_id<-trello.json.data$actions$data.organization.id
+		card_memberCreator_id<-trello.json.data$actions$memberCreator.id
+		card_memberCreator_avatarHash<-trello.json.data$actions$memberCreator.avatarHash
+		card_memberCreator_fullName<-trello.json.data$actions$memberCreator.fullName
+		card_memberCreator_initials<-trello.json.data$actions$memberCreator.initials
+		card_memberCreator_username<-trello.json.data$actions$memberCreator.username
+		card_member_id<-trello.json.data$actions$member.id
+		card_member_avatarHash<-trello.json.data$actions$member.avatarHash
+		card_member_fullName<-trello.json.data$actions$member.fullName
+		card_member_initials<-trello.json.data$actions$member.initials
+		card_member_username<-trello.json.data$actions$member.username
+
+
+
+
+		#Checklist
+
+		checklist_id<-trello.json.data$checklists$id
+		checklist_name<-trello.json.data$checklists$name
+		checklist_idBoard<-trello.json.data$checklists$idBoard
+		checklist_idCard<-trello.json.data$checklists$idCard
+		checklist_pos<-trello.json.data$checklists$pos
+		checklist_checkItems<-trello.json.data$checklists$checkItems 
+		checklist_limits_checkItems_perChecklist_status<-trello.json.data$checklists$limits.checkItems.perChecklist.status
+		checklist_limits_checkItems_perChecklist_disableAt<-trello.json.data$checklists$limits.checkItems.perChecklist.disableAt
+		checklist_limits_checkItems_perChecklist_warnAt<-trello.json.data$checklists$limits.chaeckItems.perChecklist.warnAt
+
+
+	###################
+	###bind variable###
+	###################
+
+		#Card
+		a_card_data_base<-cbind(card_id, card_idMemberCreator, card_type, card_date, card_data_text, card_data_idMember, card_data_dateLastEdited, card_data_idMemberAdded, 
+		card_data_memberType, card_data_idOriginalCommenter, card_data_deactivated, card_data_checkItem_textData, card_data_checkItem_state, card_data_checkItem_name, 
+		card_data_checkItem_id, card_data_checklist_name, card_data_checklist_id, card_data_card_shortLink, card_data_card_idShort, card_data_card_name, card_data_card_id, 
+		card_data_card_due, card_data_card_dueComplete, card_data_card_pos, card_data_card_idList, card_data_card_closed, card_data_card_desc, card_data_board_shortLink, 
+		card_data_board_name, card_data_board_id, card_data_board_prefs_permissionLevel, card_data_board_prefs_selfJoin, card_data_list_name, card_data_list_id, 
+		card_data_list_closed, card_data_list_pos, card_data_old_due, card_data_old_dueComplete, card_data_old_name, card_data_old_pos, card_data_old_idList, 
+		card_data_old_closed, card_data_old_desc, card_data_old_prefs_permissionLevel, card_data_old_prefs_selfJoin, card_data_listAfter_name, card_data_listAfter_id, 
+		card_data_listBefore_name, card_data_listBefore_id, card_data_attachment_url, card_data_attachment_name, card_data_attachment_id, card_data_attachment_previewUrl, 
+		card_data_attachment_previewUrl2x, card_data_cardSource_shortLink, card_data_cardSource_idShort, card_data_cardSource_name, card_data_cardSource_id, 
+		card_data_boardSource_id, card_data_plugin_url, card_data_plugin_public, card_data_plugin_name, card_data_plugin_id, card_data_member_name, card_data_member_id, 
+		card_data_boardTarget_id, card_data_organization_name, card_data_organization_id, card_memberCreator_id, card_memberCreator_avatarHash, card_memberCreator_fullName, 
+		card_memberCreator_initials, card_memberCreator_username, card_member_id, card_member_avatarHash, card_member_fullName, card_member_initials, card_member_username)
+
+
+
+
+
+
+		#Checklist
+		a_checklist_data_base<-cbind(checklist_id, checklist_name, checklist_idBoard, checklist_idCard, checklist_pos, checklist_checkItems, 
+		checklist_limits_checkItems_perChecklist_status, checklist_limits_checkItems_perChecklist_disableAt, checklist_limits_checkItems_perChecklist_warnAt)
+
+	################################
+	###check if have new variable###
+	################################
+
+	variable_name_on_code<-colnames(a_card_data_base)
+	variable_name_on_trello<-paste("card_",str_replace_all(names(trello.json.data$actions), "[.]", "_"), sep="")
+	i=1
+	a_error_code<-array(data = 1,dim = length(variable_name_on_code))
+	while (i <= length(variable_name_on_code)){
+		j=1
+		while (j <=length(variable_name_on_trello)){
+			if (variable_name_on_code[i] == variable_name_on_trello[j]){
+				a_error_code[i] <- 0
+				j=length(variable_name_on_trello)+1
+			} else{
+				j=j+1
+			}
+		}
+		i=i+1
+	}
+
+	i=1
+		a_error_trello<-array(data = 1,dim = length(variable_name_on_trello))
+		while (i <= length(variable_name_on_trello)){
+			j=1
+			while (j <=length(variable_name_on_code)){
+				if (variable_name_on_trello[i] == variable_name_on_code[j]){
+					a_error_trello[i] <- 0
+					j=length(variable_name_on_code)+1
+				} else{
+					j=j+1
+				}
+			}
+			i=i+1
+		}
+
+##########################
+###delete old variables###
+##########################
+a_a<-ls()
+a_ = length(ls())
+del_var<-c()
+while (a_ >= 0){
+	if(substring(a_a[a_],1,2)!="a_"){
+		del_var<-append(del_var, a_a[a_])
+#		rm(a_a[a_])
+	}
+	a_=a_-1
+} 
+rm(list = del_var)
+rm(a_)
+rm(a_a)
+
+
+if (sum(a_error_code)+sum(a_error_trello)==0){
+
+
+
+
+
+
+
+
+
+
+
+
+} else{
+	print(paste("Check the variables names, have ", sum(a_error_code)+sum(a_error_trello)," error(s). Start again",sep=""))
+}
+
+
+
+#agregate by card
+card.data.base<-sort(card.data.base, )
+card.data.base[1:15,]
+
+
+
+
+
+#after do everything for card
+#merge with another tables (like checklist)
+
+
+#use actions to know what happend and i need to find the comments in a card
+#use cards to know about the card and merge
+#use checklist to know which activity is done or not
+
+
+
+
